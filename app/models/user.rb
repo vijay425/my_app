05278a27cd73b0f9e1544cfb6572b1c236ce6ef2
Vijay Karmani <vijay.karmani@gmail.com>
2013-06-27 10:15:58 +0530

    basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  validates(:name, presence: true)
+  validates(:password, length: {minimum: 6})
+  validates(:password_confirmation, presence: true)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates(:email, presence: true, length: {maximum: 50}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false})
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+end
